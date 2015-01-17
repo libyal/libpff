bfd555fb5ea189d36d2dec6cb9b52d7643e89f0c
Joachim Metz <joachim.metz@gmail.com>
2015-01-17 11:26:31 +0100

    Worked on AppVeyor test configuration.

--- synczlib.ps1
+++ synczlib.ps1
@@ -6,34 +6,34 @@
 function DownloadFile($Url, $Destination)
 {
 	$Client = New-Object Net.WebClient
-	$Client.DownloadFile($Url, $Destination)
+	${Client}.DownloadFile(${Url}, ${Destination})
 }
 
 function ExtractZip($Zip, $Destination)
 {
 	$Shell = New-Object -ComObject Shell.Application
-	$Archive = $Shell.NameSpace($Zip)
-	$Directory = $Shell.Namespace($Destination)
+	$Archive = ${Shell}.NameSpace(${Zip})
+	$Directory = ${Shell}.Namespace(${Destination})
 
-	foreach($FileEntry in $Archive.items())
+	foreach($FileEntry in ${Archive}.items())
 	{
-		$Directory.CopyHere($FileEntry)
+		${Directory}.CopyHere(${FileEntry})
 	}
 }
 
-$Filename = "${pwd}\..\zlib128.zip"
+$Filename = "${pwd}\zlib128.zip"
 
 if (Test-Path ${Filename})
 {
 	Remove-Item -Path ${Filename} -Force
 }
-DownloadFile -Url "http://zlib.net/zlib128.zip" -Destination $Filename
+DownloadFile -Url "http://zlib.net/zlib128.zip" -Destination ${Filename}
 
 if (Test-Path "zlib-1.2.8")
 {
 	Remove-Item -Path "zlib-1.2.8" -Force -Recurse
 }
-ExtractZip -Zip $Filename -Destination "${pwd}"
+ExtractZip -Zip ${Filename} -Destination "${pwd}"
 
 if (Test-Path "..\zlib")
 {
