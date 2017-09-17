a0d674d19daacb8c32b49b8105d031066083687e
Joachim Metz <joachim.metz@gmail.com>
2017-09-17 09:18:56 +0200

    Worked on tests

--- acinclude.m4
+++ acinclude.m4
@@ -1,3 +1,7 @@
+dnl Checks for required headers and functions
+dnl
+dnl Version: 20170917
+
 dnl Function to detect if libpff dependencies are available
 AC_DEFUN([AX_LIBPFF_CHECK_LOCAL],
   [dnl Check for internationalization functions in libpff/libpff_i18n.c 
@@ -37,6 +41,16 @@ AC_DEFUN([AX_PFFTOOLS_CHECK_LOCAL],
    [AC_MSG_FAILURE(
      [Missing function: close],
      [1])
+  ])
+
+  dnl Headers included in pfftools/log_handle.c
+  AC_CHECK_HEADERS([stdarg.h varargs.h])
+
+  AS_IF(
+    [test "x$ac_cv_header_stdarg_h" != xyes && test "x$ac_cv_header_varargs_h" != xyes],
+    [AC_MSG_FAILURE(
+      [Missing headers: stdarg.h and varargs.h],
+      [1])
   ])
 
   dnl Check if tools should be build as static executables
--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,6 +1,6 @@
 # Tests C library functions and types.
 #
-# Version: 20170827
+# Version: 20170911
 
 $ExitSuccess = 0
 $ExitFailure = 1
@@ -9,7 +9,33 @@ $ExitIgnore = 77
 $LibraryTests = "attached_file_io_handle column_definition data_array data_array_entry data_block descriptors_index error index index_node index_value io_handle item item_descriptor item_values local_descriptor_node local_descriptor_value local_descriptors multi_value name_to_id_map_entry notify offsets_index record_entry record_set reference_descriptor table table_block_index table_index_value"
 $LibraryTestsWithInput = "file support"
 
-$TestToolDirectory = "..\msvscpp\Release"
+Function GetTestToolDirectory
+{
+	$TestToolDirectory = ""
+
+	ForEach (${VSDirectory} in "msvscpp vs2008 vs2010 vs2012 vs2013 vs2015 vs2017" -split " ")
+	{
+		ForEach (${VSConfiguration} in "Release VSDebug" -split " ")
+		{
+			$TestToolDirectory = "..\${VSDirectory}\${VSConfiguration}"
+
+			If (Test-Path ${TestToolDirectory})
+			{
+				Return ${TestToolDirectory}
+			}
+			ForEach (${VSPlatform} in "Win32 x64" -split " ")
+			{
+				$TestToolDirectory = "..\${VSDirectory}\${VSConfiguration}\${VSPlatform}"
+
+				If (Test-Path ${TestToolDirectory})
+				{
+					Return ${TestToolDirectory}
+				}
+			}
+		}
+	}
+	Return ${TestToolDirectory}
+}
 
 Function RunTest
 {
@@ -38,50 +64,8 @@ Function RunTest
 	Return ${Result}
 }
 
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\msvscpp\VSDebug"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2010\Release"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2010\VSDebug"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2012\Release"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2012\VSDebug"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2013\Release"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2013\VSDebug"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2015\Release"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2015\VSDebug"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2017\Release"
-}
-If (-Not (Test-Path ${TestToolDirectory}))
-{
-	$TestToolDirectory = "..\vs2017\VSDebug"
-}
+$TestToolDirectory = GetTestToolDirectory
+
 If (-Not (Test-Path ${TestToolDirectory}))
 {
 	Write-Host "Missing test tool directory." -foreground Red
@@ -93,6 +77,11 @@ $Result = ${ExitIgnore}
 
 Foreach (${TestName} in ${LibraryTests} -split " ")
 {
+	# Split will return an array of a single empty string when LibraryTests is empty.
+	If (-Not (${TestName}))
+	{
+		Continue
+	}
 	$Result = RunTest ${TestName}
 
 	If (${Result} -ne ${ExitSuccess})
@@ -103,6 +92,11 @@ Foreach (${TestName} in ${LibraryTests} -split " ")
 
 Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 {
+	# Split will return an array of a single empty string when LibraryTestsWithInput is empty.
+	If (-Not (${TestName}))
+	{
+		Continue
+	}
 	# TODO: add RunTestWithInput
 	$Result = RunTest ${TestName}
 
