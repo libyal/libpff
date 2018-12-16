322605fe037ebb180e468d5325fc4444d0d6944d
Joachim Metz <joachim.metz@gmail.com>
2018-12-16 14:21:01 +0100

    Fixed issue in fallback inflate implementation

--- libpff/libpff_deflate.c
+++ libpff/libpff_deflate.c
@@ -1540,6 +1540,11 @@ int libpff_deflate_decompress(
 				bit_stream.byte_stream_offset += block_size;
 				uncompressed_data_offset      += block_size;
 
+				/* Flush the bit-stream buffer
+				 */
+				bit_stream.bit_buffer      = 0;
+				bit_stream.bit_buffer_size = 0;
+
 				break;
 
 			case LIBPFF_DEFLATE_BLOCK_TYPE_HUFFMAN_FIXED:
@@ -1617,6 +1622,11 @@ int libpff_deflate_decompress(
 	}
 	if( ( bit_stream.byte_stream_size - bit_stream.byte_stream_offset ) >= 4 )
 	{
+		while( bit_stream.bit_buffer_size >= 8 )
+		{
+			bit_stream.byte_stream_offset -= 1;
+			bit_stream.bit_buffer_size    -= 8;
+		}
 		byte_stream_copy_to_uint32_big_endian(
 		 &( bit_stream.byte_stream[ bit_stream.byte_stream_offset ] ),
 		 stored_checksum );
