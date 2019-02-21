ecaf3470346712bd25b518c33dd7b850fafbc0f2
Zachary Travis <ztravis@everlaw.com>
2019-02-20 18:45:39 -0800

    Fix build command

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class custom_build_ext(build_ext):
       ]
 
     else:
-      command = "sh configure --disable-shared-libs"
+      command = "sh configure --disable-shared-libs --with-zlib=no"
       output = self._RunCommand(command)
 
       print_line = False
