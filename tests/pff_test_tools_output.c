4e0e08b8d27a95925864c2f30b295db7da63b002
Joachim Metz <joachim.metz@gmail.com>
2021-03-26 13:18:29 +0100

    Worked on tests

--- tests/pff_test_tools_output.c
+++ tests/pff_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../pfftools/pfftools_output.h"
 
-/* Tests the pfftools_output_initialize and function
+/* Tests the pfftools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int pff_test_tools_output_initialize(
