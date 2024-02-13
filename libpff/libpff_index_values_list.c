740ba3a691fd52249f6cca56ef7e84d111e0ded2
Joachim Metz <joachim.metz@gmail.com>
2024-02-13 05:20:53 +0100

    Applied updates

--- libpff/libpff_index_values_list.c
+++ libpff/libpff_index_values_list.c
@@ -1,7 +1,7 @@
 /*
  * Index value list functions
  *
- * Copyright (C) 2008-2023, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2024, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
